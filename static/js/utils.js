4a7682c21ca945768e5c44a150474e429a3da3a8
恒 <nh772470387@163.com>
2020-07-09 15:40:03 +0800

    Add files via upload
    更新导入功能

--- static/js/utils.js
+++ static/js/utils.js
@@ -57,11 +57,39 @@ const openDownload = (url, saveName) => {
 	aLink.dispatchEvent(event);
 }
 
+// get head from excel file,return array
+function get_header_row(XLSX, sheet) {
+    const headers = []
+    const range = XLSX.utils.decode_range(sheet['!ref'])
+    let C
+    const R = range.s.r /* start in the first row */
+    for (C = range.s.c; C <= range.e.c; ++C) { /* walk every column in the range */
+        var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })] /* find the cell in the first row */
+        var hdr = 'UNKNOWN ' + C // <-- replace with your desired default
+        if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
+        headers.push(hdr)
+    }
+    return headers
+}
+
+const read = (XLSX, data, type) => {
+    /* if type == 'base64' must fix data first */
+    // const fixedData = fixdata(data)
+    // const workbook = XLSX.read(btoa(fixedData), { type: 'base64' })
+    const workbook = XLSX.read(data, { type: type });
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+    const header = get_header_row(XLSX, worksheet);
+    const results = XLSX.utils.sheet_to_json(worksheet);
+    return {header, results};
+}
+
 export {
 	MEMO_KEYS,
 	MEMO_KEY_,
 	getTime,
 	random,
 	sheet2blob,
-	openDownload
+	openDownload,
+	read
 };
